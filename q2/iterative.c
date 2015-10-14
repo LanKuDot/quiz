3566c9691a922a8ed2f525532c51c364640ed4a6
LanKuDot <lankudot@github.com>
2015-10-14 23:21:05 +0800

    q2: Get input string from command line

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,7 @@
 #include "smallest_char.h"
 
-char smallest_character( char str[], char c ) {
+char smallest_character( char str[], char c )
+{
 	const char *strptr = str;
 	while( *strptr != '\0' ) {
 		if ( *strptr > c )
--- q2/main.c
+++ q2/main.c
@@ -1,11 +1,14 @@
+#include <assert.h>
 #include <stdio.h>
 
 #include "smallest_char.h"
 
-int main() {
-	char input1[] = "abcdefghijklmnopqrstuvwxyz";
+int main( int argc, char *argv[] )
+{
+	/* There must be 3 arguments. */
+	assert( argc == 3 && "Input format: <program> <sorted_str> <search character>" );
 
-	printf( "%c\n", smallest_character( input1, '\0' ) );
+	printf( "%c\n", smallest_character( argv[1], argv[2][0] ) );
 
 	return 0;
 }
