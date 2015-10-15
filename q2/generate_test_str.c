5f21575323d15ebe44bdedb0c247fc37128dd8f9
LanKuDot <lankudot@github.com>
2015-10-15 21:58:35 +0800

    q2: Add random sorted-string generator

---
+++ q2/generate_test_str.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main( int argc, char *argv[] )
+{
+	char charBank[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char str[53+1];    // 1 for null character.
+	int len = 0;
+
+	srand( time(NULL) );
+	for ( int i = 0; i < 53; ++i ) {
+		if ( rand() & 1 )
+			str[len++] = charBank[i];
+	}
+	str[len] = '\0';
+
+	printf( "%s %c\n", str, charBank[ rand() % 52 ] );
+
+	return 0;
+}
