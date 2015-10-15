26e3cdb5e498b17cf11ef0b40f13664213e3f4ea
LanKuDot <lankudot@github.com>
2015-10-15 21:59:21 +0800

    q2: Check input character if is null

--- q2/iterative.c
+++ q2/iterative.c
@@ -3,6 +3,10 @@
 char smallest_character( char str[], char c )
 {
 	const char *strptr = str;
+
+	if ( c == '\0' )
+		return str[0];
+
 	while( *strptr != '\0' ) {
 		if ( *strptr > c )
 			return *strptr;
