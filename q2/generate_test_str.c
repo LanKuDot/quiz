f51be3d9ba93111d9c5e0fc5413d1b0ececab74a
LanKuDot <lankudot@github.com>
2015-10-16 22:20:14 +0800

    q2: Decide one character for more times which can generate more longer string.

--- q2/generate_test_str.c
+++ q2/generate_test_str.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 
+/* argv[1]: the times of each character decided.
+ */
 int main( int argc, char *argv[] )
 {
 	char charBank[53] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char str[53+1];    // 1 for null character.
-	int len = 0;
+	char str[52 * atoi(argv[1]) + 1];    // 1 for null character.
+	int len = 0, times = atoi( argv[1] );
 
 	srand( time(NULL) );
-	for ( int i = 0; i < 53; ++i ) {
+	for ( int i = 0; i < 52 * times + 1; ++i ) {
 		if ( rand() & 1 )
-			str[len++] = charBank[i];
+			str[len++] = charBank[i/times];
 	}
 	str[len] = '\0';
 
