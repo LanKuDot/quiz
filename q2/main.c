6b259cc9fc8050804f45a04dab9ee7ea3d403d37
LanKuDot <lankudot@github.com>
2015-10-17 00:02:03 +0800

    q2: Determine that whether to output exec. time or not

--- q2/main.c
+++ q2/main.c
@@ -19,17 +19,21 @@ static double diff_in_second( struct timespec t1, struct timespec t2 )
 
 int main( int argc, char *argv[] )
 {
-	struct timespec start, end;
-
 	/* There must be 3 arguments. */
 	assert( argc == 3 && "Input format: <program> <sorted_str> <search character>" );
 	
+#ifdef MEASURE_TIME
+	struct timespec start, end;
+
 	clock_gettime( CLOCK_REALTIME, &start );
 	smallest_character( argv[1], argv[2][0] );
 	clock_gettime( CLOCK_REALTIME, &end );
+#endif
 
 	printf( "%c\n", smallest_character( argv[1], argv[2][0] ) );
+#ifdef MEASURE_TIME
 	printf( "Time for smallest_character(): %.9lf sec\n", diff_in_second( start, end ) );
+#endif
 
 	return 0;
 }
