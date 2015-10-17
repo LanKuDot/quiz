53c6eb39d141640ce0e52a0d9c9d5aa0eb373424
LanKuDot <lankudot@github.com>
2015-10-17 21:45:36 +0800

    q4: Add iterative version

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,23 @@
-/* FIXME: Implement! */
+#include <stdio.h>
 
-int main()
+#include "max_sub_array.h"
+
+int maxSubArray( int A[], int n )
 {
-    return 0;
+	if ( A == NULL )
+		return 0;
+	if ( n == 0 )
+		return 0;
+	if ( n == 1 )
+		return A[0];
+
+	int localmax, max;
+	localmax = max = A[0];
+
+	for ( int i = 1; i < n; ++i ) {
+		localmax = ( localmax + A[i] > 0 ) ? ( localmax + A[i] ) : 0;
+		max = ( localmax > max ) ? localmax : max;
+	}
+
+	return max;
 }
