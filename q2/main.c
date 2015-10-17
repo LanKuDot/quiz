754d7807ab9fe32373c94b6b6667f1f3531f1f1a
LanKuDot <lankudot@github.com>
2015-10-17 23:07:25 +0800

    Extract function diff_in_second

--- q2/main.c
+++ q2/main.c
@@ -3,19 +3,9 @@
 #include <time.h>
 
 #include "smallest_char.h"
-
-static double diff_in_second( struct timespec t1, struct timespec t2 )
-{
-	struct timespec diff;
-	if ( t2.tv_nsec - t1.tv_nsec < 0 ) {
-		diff.tv_sec = t2.tv_sec - t1.tv_sec - 1;
-		diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
-	} else {
-		diff.tv_sec = t2.tv_sec - t1.tv_sec;
-		diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
-	}
-	return ( diff.tv_sec + diff.tv_nsec / 1000000000.0 );
-}
+#ifdef MEASURE_TIME
+	#include "../diff_in_second.h"
+#endif
 
 int main( int argc, char *argv[] )
 {
