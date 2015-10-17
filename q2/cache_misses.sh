fd686b50c22fbc80dc21863854b59763d25143f2
LanKuDot <lankudot@github.com>
2015-10-17 15:14:45 +0800

    q2: Report cache-misses event

---
+++ q2/cache_misses.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+test_str=$( ./generate_test_str 50 )
+echo "Test string: $test_str"
+echo "Searching character: y"
+echo "echo 1 > /proc/sys/vm/drop_caches" | sudo sh
+sudo perf stat -e cache-misses,cache-references ./iterative $test_str 'y'
+echo "echo 1 > /proc/sys/vm/drop_caches" | sudo sh
+sudo perf stat -e cache-misses,cache-references ./iterative_2 $test_str 'y'
