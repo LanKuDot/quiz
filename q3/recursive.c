8dddf3eb31e06ee2f4daff53792882eaad72c9c9
LanKuDot <lankudot@github.com>
2015-10-22 00:35:19 +0800

    q3: Create recursive version

--- q3/recursive.c
+++ q3/recursive.c
@@ -1,6 +1,34 @@
-/* FIXME: Implement! */
+#include <stdlib.h>
 
-int main()
+#include "flatten.h"
+
+TreeNode *doFlat( TreeNode *node );
+
+void flatten( TreeNode *root )
 {
-    return 0;
+	if ( root == NULL )
+		return;
+
+	doFlat( root );
+}
+
+TreeNode *doFlat( TreeNode *node )
+{
+	TreeNode *carry = NULL;
+
+	if ( node->right != NULL )
+		carry = node->right;
+
+	if ( node->left != NULL ) {
+		node->right = node->left;
+		node->left = NULL;
+		node = doFlat( node->right );
+	}
+
+	if ( carry != NULL ) {
+		node->right = carry;
+		node = doFlat( node->right );
+	}
+
+	return node;
 }
