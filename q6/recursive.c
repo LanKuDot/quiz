720e2ee21f4c76cb494be519dfb3bc24477b467f
lankudot <lankudot@github.com>
2015-10-30 23:26:39 +0800

    q6: Create recursive version

--- q6/recursive.c
+++ q6/recursive.c
@@ -1,6 +1,57 @@
-/* FIXME: Implement! */
+#include <stdlib.h>
+#include "reverseBetween.h"
 
-int main()
+ListNode *doReverse( ListNode *head );
+
+ListNode *reverseBetween( ListNode *head, int m, int n )
 {
-    return 0;
+	if ( m == n ) return head;
+
+	// Extract the sub linked list which would be reversed.
+	ListNode *connect, *subHead, *tail, *tmp;
+	int i;
+
+	connect = head;
+
+	// Find the first element in the reverse interval and
+	// the last element before the reverse interval
+	for ( i = 1; i < m-1; ++i, connect = connect->next )
+		;
+
+	// Find the last element in the reverse interval
+	tmp = connect;
+	for ( ; i < n; ++i, tmp = tmp->next )
+		;
+	// Store the next element behind the reverse interval
+	tail = tmp->next;
+	// Cut!
+	tmp->next = NULL;
+
+	if ( m != 1 ) {
+		subHead = connect->next;
+		connect->next = doReverse( subHead );
+	} else
+		head = doReverse( head );
+
+	subHead->next = tail;
+
+	return head;
+}
+
+ListNode *doReverse( ListNode *head )
+{
+	// No element there.
+	if ( head == NULL ) return NULL;
+	// Only one element.
+	if ( head->next == NULL ) return head;
+
+	ListNode *right = head->next, *newHead;
+
+	head->next = NULL;
+
+	newHead = doReverse( right );
+
+	right->next = head;
+
+	return newHead;
 }
