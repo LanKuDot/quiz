deee1473ee880dd9ffc07c6a5d51a6624bb77f42
lankudot <lankudot@github.com>
2015-10-30 15:52:51 +0800

    q6: Add include guards

--- q6/reverseBetween.h
+++ q6/reverseBetween.h
@@ -1,6 +1,11 @@
+#ifndef REV_BETWEEN
+#define REV_BETWEEN
+
 typedef struct NODE {
 	int data;
 	struct NODE* next;
 } ListNode;
 
 ListNode *reverseBetween( ListNode *head, int m, int n );
+
+#endif
